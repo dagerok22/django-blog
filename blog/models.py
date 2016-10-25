664096d132fee1663feaca88a2cef6daaca932d6
dagerok22 <dagerok22@gmail.com>
2016-10-25 17:57:56 +0700

    Some fitures added

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_short_content(self):
+        return self.content[:500]
+
+    def get_absolute_path(self):
+        return reverse("posts:detail", kwargs={"id": self.id})
---
+++ blog/views.py
@@ -0,0 +1,65 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib import messages
+
+# Create your views here.
+from django.views.decorators.csrf import csrf_protect
+
+from .models import Post
+from .forms import PostForm
+
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Successfully created")
+        return HttpResponseRedirect(instance.get_absolute_path())
+    else:
+        messages.success(request, "Error")
+
+    context = {
+        "form": form
+    }
+    return render(request, "post_form.html", context)
+
+def post_detail(request, id):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance,
+    }
+    return render(request, "post_detail.html", context)
+
+def post_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "post_list": queryset,
+        "title": "List"
+    }
+    return render(request, "post_list.html", context)
+
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_path())
+
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully deleted")
+    return redirect("posts:list")
+
+
