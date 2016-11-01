76c6cd1f56f3597a37b345b58194dbf42ee2cebb
dagerok22 <dagerok22@gmail.com>
2016-11-01 19:34:41 +0700

    added pagination and image field

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib import messages
 
+
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+
 # Create your views here.
 from django.views.decorators.csrf import csrf_protect
 
@@ -11,7 +15,7 @@ from .forms import PostForm
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -34,16 +38,30 @@ def post_detail(request, id):
     return render(request, "post_detail.html", context)
 
 def post_list(request):
-    queryset = Post.objects.all().order_by("-timestamp")
+    queryset_list = Post.objects.all().order_by("-timestamp")
+    paginator = Paginator(queryset_list, 5)  # Show 25 contacts per page
+
+    page_request_var = "page"
+    page = request.GET.get('page')
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {
         "post_list": queryset,
-        "title": "List"
+        "title": "List",
+        "page_request_var": page_request_var,
     }
     return render(request, "post_list.html", context)
 
+
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
