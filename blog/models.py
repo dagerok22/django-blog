fe1255b29fc32b7b97d3affdd356dde63cf2b264
dagerok22 <dagerok22@gmail.com>
2016-11-06 21:31:10 +0700

    Users, share links and add button

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.db.models.signals import pre_save
 
+from django.conf import settings
 # Create your models here.
 from django.urls import reverse
-
+from django.utils.text import slugify
 
 class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
     image = models.FileField(null=True, blank=True)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -18,4 +22,21 @@ class Post(models.Model):
         return self.content[:500]
 
     def get_absolute_path(self):
-        return reverse("posts:detail", kwargs={"id": self.id})
+        return reverse("posts:detail", kwargs={"slug": self.slug})
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+def pre_save_post_reciver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_reciver, sender=Post)
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib import messages
+
+from urllib.parse import quote_plus
 
 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -15,9 +18,14 @@ from .forms import PostForm
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    if not request.user.is_authenticated():
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Successfully created")
         return HttpResponseRedirect(instance.get_absolute_path())
@@ -29,8 +37,8 @@ def post_create(request):
     }
     return render(request, "post_form.html", context)
 
-def post_detail(request, id):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug):
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title": instance.title,
         "instance": instance,
@@ -59,8 +67,10 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -74,8 +84,10 @@ def post_update(request, id=None):
     }
     return render(request, "post_form.html", context)
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted")
     return redirect("posts:list")
